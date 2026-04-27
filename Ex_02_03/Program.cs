061f4093b98e77517d56aa3eb775c187b7fb5e9f
mizuto26 <192568839+mizuto26@users.noreply.github.com>
2026-04-27 15:40:48 +0900

    #演習3

---
+++ Ex_02_03/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Ex_02_03;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("数値を入力してください->");
+        int value = int.Parse(Console.ReadLine()!);
+        Console.Write($"5を加算した結果は{value + 5}です");
+        Console.Write($"5を減算した結果は{value - 5}です");
+    }
+}
