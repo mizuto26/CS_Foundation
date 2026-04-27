c6f6f8123848824406ede0f465df0bf50f6c40d0
mizuto26 <192568839+mizuto26@users.noreply.github.com>
2026-04-27 16:54:16 +0900

    #演習11

---
+++ Ex_02_11/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex_02_11;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        string[] values = { "品川", "大崎", "五反田", "目黒", "恵比寿", "渋谷", "原宿", "代々木", "新宿" };
+        Console.WriteLine("駅名を入力してください->");
+        string name = Console.ReadLine();
+
+        for (int i = 0; i < values.Length; i++)
+        {
+            if (values[i] == name)
+            {
+                continue;
+            }
+            Console.WriteLine(values[i]);
+        }
+    }
+}
