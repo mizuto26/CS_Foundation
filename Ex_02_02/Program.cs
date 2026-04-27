c475b87556edf28e0be790c836209f53588dc624
mizuto26 <192568839+mizuto26@users.noreply.github.com>
2026-04-27 15:34:21 +0900

    #演習2

---
+++ Ex_02_02/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Ex_02_02;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("文字列を入力してください->");
+        string value = Console.ReadLine();
+        Console.WriteLine(value);
+    }
+}
