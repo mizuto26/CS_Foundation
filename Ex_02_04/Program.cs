abc7e9d8fc5e4361475178dbb16c0e6e2cb64210
mizuto26 <192568839+mizuto26@users.noreply.github.com>
2026-04-27 15:48:40 +0900

    #演習４

---
+++ Ex_02_04/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ex_02_04;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = new int[3];
+
+        Console.WriteLine("数値1を入力してください->");
+        array[0] = int.Parse(Console.ReadLine()!);
+        Console.WriteLine("数値2を入力してください->");
+        array[1] = int.Parse(Console.ReadLine()!);
+        Console.WriteLine("数値3を入力してください->");
+        array[2] = int.Parse(Console.ReadLine()!);
+
+        Console.WriteLine("入力結果");
+
+        Console.Write($"数値1:{array[0]}\n");
+        Console.Write($"数値2:{array[1]}\n");
+        Console.Write($"数値3:{array[2]}\n");
+    }
+}
