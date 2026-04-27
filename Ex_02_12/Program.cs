33fc6678a88f338b728e989a9029e10d19d5e2c1
mizuto26 <192568839+mizuto26@users.noreply.github.com>
2026-04-27 17:02:19 +0900

    #演習12

---
+++ Ex_02_12/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Ex_02_12;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("値を入力して下さい->");
+        int number = Mul3(int.Parse(Console.ReadLine()!));
+        Console.Write(number);
+    }
+
+    static int Mul3(int value)
+    {
+        return value * 3;
+    }
+}
