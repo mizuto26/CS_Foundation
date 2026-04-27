2895a239bb562b8f18d087541005c0cc5974ac40
mizuto26 <192568839+mizuto26@users.noreply.github.com>
2026-04-27 16:40:03 +0900

    #演習8

---
+++ Ex_02_08/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Ex_02_08;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        string[] value = ["品川", "大崎", "五反田", "目黒", "恵比寿", "渋谷", "原宿", "代々木", "新宿"];
+        int count = 0;
+
+        while (count < value.Length)
+        {
+            Console.WriteLine(value[count]);
+            count++;
+        }
+    }
+}
