e7d0716241ee1edd02780c53f366ade136ff96c3
mizuto26 <192568839+mizuto26@users.noreply.github.com>
2026-04-27 11:32:02 +0900

    first commit

--- Ex_02_01/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ex_02_01;
-
-static class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
---
+++ Example/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace Example;
+
+static class Program{
+    static void Main(string[] args){
+        Console.WriteLine("Hello, World!");
+    }
+}
