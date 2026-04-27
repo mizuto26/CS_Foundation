8645dfd45ed5590617eda76abfece51008ff9d3e
mizuto26 <192568839+mizuto26@users.noreply.github.com>
2026-04-27 16:24:51 +0900

    #演習7

---
+++ Ex_02_07/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ex_02_07;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("数値を入力してください->");
+        int value = int.Parse(Console.ReadLine()!);
+
+        if (value == 0)
+        {
+            Console.WriteLine("0です");
+        }
+        else if (value > 0)
+        {
+            Console.WriteLine("正の値です");
+        }
+        else if (value < 0)
+        {
+            Console.WriteLine("負の値です");
+        }
+    }
+}
