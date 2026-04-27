2adaebaf6436ab4c81f5d0d13d0c53d2f6afbd6c
mizuto26 <192568839+mizuto26@users.noreply.github.com>
2026-04-27 16:16:22 +0900

    #演習５

---
+++ Ex_02_05/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ex_02_05;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("数値を入力してください->");
+        int value = int.Parse(Console.ReadLine()!);
+
+        if (value % 2 == 0)
+        {
+            Console.WriteLine($"結果:{value * 10}");
+        }
+        else
+        {
+            Console.WriteLine($"結果:{value}");
+        }
+    }
+}
