daa8cd34aea2cabb645b85e3086abc68790465ab
mizuto26 <192568839+mizuto26@users.noreply.github.com>
2026-04-27 13:41:33 +0900

    #3

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,7 +1,12 @@
 ﻿namespace Example;
 
-static class Program{
-    static void Main(string[] args){
-        Console.WriteLine("Hello, World!");
+static class Program
+{
+    static void Main(string[] args)
+    {
+        int quantity = 3;
+        int price = 2;
+        int total = quantity * price;
+        System.Console.WriteLine(total);
     }
 }
