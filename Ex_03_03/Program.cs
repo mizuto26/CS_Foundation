2e47ca9ad6915b2062c31951211beacadc0c979c
mizuto26 <192568839+mizuto26@users.noreply.github.com>
2026-04-28 10:38:58 +0900

    #演習3

--- Ex_03_03/Program.cs
+++ Ex_03_03/Program.cs
@@ -3,9 +3,12 @@
 Console.WriteLine("商品番号を入力してください->");
 item1.itemNo = int.Parse(Console.ReadLine()!);
 Console.WriteLine("商品名を入力して下さい->");
-item1.itemName = Console.ReadLine();
+item1.itemName = Console.ReadLine()!;
 Console.WriteLine("単価を入力してください->");
 item1.price = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("新しい単価を入力してください->");
+ChangePrice(int.Parse(Console.ReadLine()!));
 
 Console.WriteLine("結果");
 
@@ -13,9 +16,17 @@ Console.WriteLine($"商品番号 = {item1.itemNo}");
 Console.WriteLine($"商品名 = {item1.itemName}");
 Console.WriteLine($"単価 = {item1.price}");
 
+void ChangePrice(int pprice)
+{
+    item1.price = pprice;
+}
+
 public class Item
 {
     public int itemNo;
     public string itemName = string.Empty;
     public int price;
 }
+
+
+
