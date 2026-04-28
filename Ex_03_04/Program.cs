7ff1d4d1888292b9405370090c375714b888bff1
mizuto26 <192568839+mizuto26@users.noreply.github.com>
2026-04-28 10:50:38 +0900

    #演習4

---
+++ Ex_03_04/Program.cs
@@ -0,0 +1,23 @@
+﻿Item item1 = new();
+
+Console.WriteLine("商品番号を入力してください->");
+item1.itemNo = int.Parse(Console.ReadLine()!);
+Console.WriteLine("商品名を入力して下さい->");
+item1.itemName = Console.ReadLine()!;
+Console.WriteLine("単価を入力してください->");
+item1.price = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"税込み金額={CalculateTaxAmount()}");
+
+int CalculateTaxAmount()
+{
+    return (int)(item1.price * (1.0 + item1.salesTaxRate));
+}
+
+public class Item
+{
+    public int itemNo;
+    public string itemName = string.Empty;
+    public int price;
+    public double salesTaxRate = 0.10;
+}
