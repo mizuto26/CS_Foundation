c92a3a450715eadf912a1aff8819ac375db7ce69
mizuto26 <192568839+mizuto26@users.noreply.github.com>
2026-04-28 10:59:27 +0900

    #演習5

---
+++ Ex_03_05/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("商品番号を入力してください->");
+int pitemNo = int.Parse(Console.ReadLine()!);
+Console.WriteLine("商品名を入力して下さい->");
+string pitemName = Console.ReadLine()!;
+Console.WriteLine("単価を入力してください->");
+int price = int.Parse(Console.ReadLine()!);
+
+Item item1 = new(pitemNo, pitemName, price);
+
+Console.WriteLine("結果");
+
+Console.WriteLine($"商品番号 = {item1.itemNo}");
+Console.WriteLine($"商品名 = {item1.itemName}");
+Console.WriteLine($"単価 = {item1.price}");
+
+public class Item
+{
+    public int itemNo;
+    public string itemName = string.Empty;
+    public int price;
+
+    public Item(int pitemNo, string pitemName, int pprice)
+    {
+        this.itemNo = pitemNo;
+        this.itemName = pitemName;
+        this.price = pprice;
+    }
+}
