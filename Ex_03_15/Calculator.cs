b20ba8f158ad1bd55eb778af005d2c4ae920388f
mizuto26 <192568839+mizuto26@users.noreply.github.com>
2026-04-30 11:17:21 +0900

    #演習15

---
+++ Ex_03_15/Calculator.cs
@@ -0,0 +1,10 @@
+namespace Ex_03_15
+{
+    public class Calculator
+    {
+        public static int Div(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ Ex_03_15/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex_03_15;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("値1を入力してください->");
+        int number1 = int.Parse(Console.ReadLine()!);
+        Console.WriteLine("値2を入力してください->");
+        int number2 = int.Parse(Console.ReadLine()!);
+        try
+        {
+            Console.WriteLine($"値1 / 値2 = {Calculator.Div(x: number1, y: number2)}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"例外:{e.Message}");
+        }
+    }
+}
