958fdd827cc6ab5d63dcca249975a77c459c1d11
mizuto26 <192568839+mizuto26@users.noreply.github.com>
2026-05-01 11:01:47 +0900

    #演習1

---
+++ Ex_04_01/Program.cs
@@ -0,0 +1,24 @@
+﻿using Ex_04_01_Lib;
+
+namespace Ex_04_01;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Item item1 = new();
+
+        Console.WriteLine("商品番号を入力してください->");
+        item1.ItemNo = int.Parse(Console.ReadLine()!);
+        Console.WriteLine("商品名を入力して下さい->");
+        item1.ItemName = Console.ReadLine();
+        Console.WriteLine("単価を入力してください->");
+        item1.Price = int.Parse(Console.ReadLine()!);
+
+        Console.WriteLine("結果");
+
+        Console.WriteLine($"商品番号 = {item1.ItemNo}");
+        Console.WriteLine($"商品名 = {item1.ItemName}");
+        Console.WriteLine($"単価 = {item1.Price}");
+    }
+}
---
+++ Ex_04_01_Lib/Item.cs
@@ -0,0 +1,9 @@
+namespace Ex_04_01_Lib
+{
+    public class Item
+    {
+        public int ItemNo { get; set; }
+        public string? ItemName { get; set; }
+        public int Price { get; set; }
+    }
+}
