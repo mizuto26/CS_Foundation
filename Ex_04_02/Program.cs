6d82d81e1951873db6a8be5d807d7a190ef609b5
mizuto26 <192568839+mizuto26@users.noreply.github.com>
2026-05-01 11:24:36 +0900

    #演習2

---
+++ Ex_04_02/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ex_04_02;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("年を入力して下さい->");
+        int year = int.Parse(Console.ReadLine()!);
+        bool IsLeapYear = DateTime.IsLeapYear(year);
+        if (IsLeapYear)
+        {
+            Console.WriteLine($"{year}年は閏年です。");
+        }
+        else
+        {
+            Console.WriteLine($"{year}年は閏年ではありません。");
+        }
+    }
+}
